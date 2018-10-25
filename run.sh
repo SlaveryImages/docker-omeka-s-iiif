1f24e5f37ad7f9b831ad2cfd0ff9a5ecbc5d292e
Sara Carlstead Brumfield <saracarl@gmail.com>
2018-10-25 09:23:06 -0500

    Added run file

---
+++ run.sh
@@ -0,0 +1,9 @@
+docker run -d --rm --name slaveryimages -p 80:80 \
+-v /c/Users/Brumfield/docker-omeka-s-iiif/database.ini:/var/www/html/config/database.ini  \
+-v /c/Users/Brumfield/omeka_files:/var/www/html/files \
+-v /c/Users/Brumfield/omeka_logs:/var/www/html/logs  \
+-v /c/Users/Brumfield/omeka-s-theme-slavery-images:/var/www/html/themes/omeka-s-theme-slavery-images \
+-v /c/Users/Brumfield/sideload_images:/var/www/html/sideload_images \
+-v /c/Users/Brumfield/CSVImport:/var/www/html/modules/CSVImport \
+slaveryimages --net="host"
+# test http://192.168.99.100/s/slaveryimages/item/132#?c=0&m=0&s=0&cv=0&xywh=-568%2C-110%2C3749%2C2086
